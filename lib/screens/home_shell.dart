242ba1a4155016bcd2b531ddc1e7d5c0773f9650
kublae1 <kublae@gmail.com>
2026-09-23 17:05:45 +0200

    Add lib/screens/home_shell.dart

---
+++ lib/screens/home_shell.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'start_screen.dart';
+import 'news_screen.dart';
+import 'events_screen.dart';
+import 'members_screen.dart';
+import 'more_screen.dart';
+
+class HomeShell extends StatefulWidget {
+  const HomeShell({super.key});
+
+  @override
+  State<HomeShell> createState() => _HomeShellState();
+}
+
+class _HomeShellState extends State<HomeShell> {
+  int index = 0;
+
+  final pages = const [
+    StartScreen(),
+    NewsScreen(),
+    EventsScreen(),
+    MembersScreen(),
+    MoreScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: pages[index]),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: index,
+        onDestinationSelected: (value) => setState(() => index = value),
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home_outlined), selectedIcon: Icon(Icons.home), label: 'Start'),
+          NavigationDestination(icon: Icon(Icons.article_outlined), selectedIcon: Icon(Icons.article), label: 'News'),
+          NavigationDestination(icon: Icon(Icons.calendar_month_outlined), selectedIcon: Icon(Icons.calendar_month), label: 'Termine'),
+          NavigationDestination(icon: Icon(Icons.groups_outlined), selectedIcon: Icon(Icons.groups), label: 'Mitglieder'),
+          NavigationDestination(icon: Icon(Icons.more_horiz), label: 'Mehr'),
+        ],
+      ),
+    );
+  }
+}
