4721488d413263f29eea9d99ec5cff58f4175cbd
kublae1 <kublae@gmail.com>
2026-09-23 19:37:46 +0200

    Update widget test for version 0.4

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flapamamaku_app/main.dart';
 
 void main() {
-  testWidgets('FLAPAMAMAKU app starts with version 0.3 content', (tester) async {
+  testWidgets('FLAPAMAMAKU app starts with version 0.4 navigation', (tester) async {
     await tester.pumpWidget(const FlapamamakuApp());
 
     expect(find.text('FLAPAMAMAKU'), findsOneWidget);
@@ -10,5 +10,6 @@ void main() {
     expect(find.text('News'), findsOneWidget);
     expect(find.text('Termine'), findsOneWidget);
     expect(find.text('Mitglieder'), findsOneWidget);
+    expect(find.text('Mehr'), findsOneWidget);
   });
 }
