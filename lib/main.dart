43e34ddc23e8a6639ebf6547b196f2fe8512b89c
kublae1 <kublae@gmail.com>
2026-09-23 19:47:01 +0200

    Wire central app store into application

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,42 @@
 import 'package:flutter/material.dart';
+import 'data/app_store.dart';
 import 'screens/home_shell.dart';
 
 void main() {
   runApp(const FlapamamakuApp());
 }
 
-class FlapamamakuApp extends StatelessWidget {
+class FlapamamakuApp extends StatefulWidget {
   const FlapamamakuApp({super.key});
+
+  @override
+  State<FlapamamakuApp> createState() => _FlapamamakuAppState();
+}
+
+class _FlapamamakuAppState extends State<FlapamamakuApp> {
+  final AppStore store = AppStore();
 
   @override
   Widget build(BuildContext context) {
     const burgundy = Color(0xFF8A101B);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'FLAPAMAMAKU',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: burgundy),
-        useMaterial3: true,
-        scaffoldBackgroundColor: const Color(0xFFF7F5F2),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: burgundy,
-          foregroundColor: Colors.white,
-          centerTitle: true,
+
+    return AppStoreScope(
+      store: store,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'FLAPAMAMAKU',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: burgundy),
+          useMaterial3: true,
+          scaffoldBackgroundColor: const Color(0xFFF7F5F2),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: burgundy,
+            foregroundColor: Colors.white,
+            centerTitle: true,
+          ),
         ),
+        home: const HomeShell(),
       ),
-      home: const HomeShell(),
     );
   }
 }
