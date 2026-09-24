67d5c3efd68b8e2cbcbdc39fac2a3a66b765d3e5
kublae1 <kublae@gmail.com>
2026-09-24 22:03:32 +0200

    branding: match mockup bottom navigation

--- lib/screens/home_shell.dart
+++ lib/screens/home_shell.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'start_screen.dart';
 import 'news_screen.dart';
 import 'events_screen.dart';
-import 'members_screen.dart';
+import 'remote_content_screen.dart';
 import 'more_screen.dart';
 
 class HomeShell extends StatefulWidget {
@@ -19,7 +19,12 @@ class _HomeShellState extends State<HomeShell> {
     StartScreen(),
     NewsScreen(),
     EventsScreen(),
-    MembersScreen(),
+    RemoteContentScreen(
+      section: 'photos',
+      title: 'Galerie',
+      emptyText: 'Noch keine Fotos oder Alben hinterlegt.',
+      icon: Icons.photo_library_outlined,
+    ),
     MoreScreen(),
   ];
 
@@ -32,11 +37,31 @@ class _HomeShellState extends State<HomeShell> {
         selectedIndex: index,
         onDestinationSelected: (value) => setState(() => index = value),
         destinations: const [
-          NavigationDestination(icon: Icon(Icons.home_outlined), selectedIcon: Icon(Icons.home_rounded), label: 'Start'),
-          NavigationDestination(icon: Icon(Icons.article_outlined), selectedIcon: Icon(Icons.article_rounded), label: 'News'),
-          NavigationDestination(icon: Icon(Icons.calendar_month_outlined), selectedIcon: Icon(Icons.calendar_month_rounded), label: 'Termine'),
-          NavigationDestination(icon: Icon(Icons.groups_outlined), selectedIcon: Icon(Icons.groups_rounded), label: 'Mitglieder'),
-          NavigationDestination(icon: Icon(Icons.more_horiz), selectedIcon: Icon(Icons.more_horiz_rounded), label: 'Mehr'),
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home_rounded),
+            label: 'Start',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.article_outlined),
+            selectedIcon: Icon(Icons.article_rounded),
+            label: 'News',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.calendar_month_outlined),
+            selectedIcon: Icon(Icons.calendar_month_rounded),
+            label: 'Termine',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.photo_outlined),
+            selectedIcon: Icon(Icons.photo_rounded),
+            label: 'Galerie',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.more_horiz),
+            selectedIcon: Icon(Icons.more_horiz_rounded),
+            label: 'Mehr',
+          ),
         ],
       ),
     );
