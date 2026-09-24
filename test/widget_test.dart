78208ee1859bdca644c31ec262894345fd007aab
kublae1 <kublae@gmail.com>
2026-09-24 21:38:32 +0200

    test: update startup expectations for new branding

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,11 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flapamamaku_app/main.dart';
 
 void main() {
-  testWidgets('FLAPAMAMAKU app starts with main navigation', (tester) async {
+  testWidgets('FLAPAMAMAKU branded app starts with main navigation', (tester) async {
     await tester.pumpWidget(const FlapamamakuApp());
 
-    expect(find.text('FLAPAMAMAKU'), findsNothing);
-    expect(find.text('Zäme ade Fasnacht Luzern!'), findsNothing);
+    expect(find.text('FLAPAMAMAKU'), findsOneWidget);
+    expect(find.text('DIE SCHWEINE ROCKER'), findsOneWidget);
     expect(find.text('News'), findsOneWidget);
     expect(find.text('Termine'), findsOneWidget);
     expect(find.text('Mitglieder'), findsOneWidget);
