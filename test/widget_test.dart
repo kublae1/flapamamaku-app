ecaa79ea77672ace9f13892b3a5b0f2c2f662f8a
kublae1 <kublae@gmail.com>
2026-09-24 22:04:24 +0200

    test: align navigation expectations with approved mockup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,14 +2,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flapamamaku_app/main.dart';
 
 void main() {
-  testWidgets('FLAPAMAMAKU branded app starts with main navigation', (tester) async {
+  testWidgets('FLAPAMAMAKU mockup navigation is available', (tester) async {
     await tester.pumpWidget(const FlapamamakuApp());
 
     expect(find.text('FLAPAMAMAKU'), findsOneWidget);
-    expect(find.text('DIE SCHWEINE ROCKER'), findsOneWidget);
+    expect(find.text('Jahresmotto'), findsOneWidget);
+    expect(find.text('Start'), findsOneWidget);
     expect(find.text('News'), findsOneWidget);
     expect(find.text('Termine'), findsOneWidget);
-    expect(find.text('Mitglieder'), findsOneWidget);
+    expect(find.text('Galerie'), findsOneWidget);
     expect(find.text('Mehr'), findsOneWidget);
   });
 }
