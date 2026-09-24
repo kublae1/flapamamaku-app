f1b9465a3f209100f7e2b961002641ac4e4a8cfb
kublae1 <kublae@gmail.com>
2026-09-24 06:58:05 +0200

    Update start screen title expectation

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@ void main() {
   testWidgets('FLAPAMAMAKU app starts with main navigation', (tester) async {
     await tester.pumpWidget(const FlapamamakuApp());
 
-    expect(find.text('FLAPAMAMAKU'), findsOneWidget);
+    expect(find.text('FLAPAMAMAKU'), findsNothing);
     expect(find.text('Zäme ade Fasnacht Luzern!'), findsNothing);
     expect(find.text('News'), findsOneWidget);
     expect(find.text('Termine'), findsOneWidget);
