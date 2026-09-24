5445c6ad33395f61baba420324b7d93dd64234d0
kublae1 <kublae@gmail.com>
2026-09-24 06:56:00 +0200

    Update start screen widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,11 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flapamamaku_app/main.dart';
 
 void main() {
-  testWidgets('FLAPAMAMAKU app starts with version 0.6 navigation', (tester) async {
+  testWidgets('FLAPAMAMAKU app starts with main navigation', (tester) async {
     await tester.pumpWidget(const FlapamamakuApp());
 
     expect(find.text('FLAPAMAMAKU'), findsOneWidget);
-    expect(find.text('Zäme ade Fasnacht Luzern!'), findsOneWidget);
+    expect(find.text('Zäme ade Fasnacht Luzern!'), findsNothing);
     expect(find.text('News'), findsOneWidget);
     expect(find.text('Termine'), findsOneWidget);
     expect(find.text('Mitglieder'), findsOneWidget);
